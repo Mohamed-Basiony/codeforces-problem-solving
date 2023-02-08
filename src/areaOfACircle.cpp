db27cfa32d6d7c4701058d00c9cf98cabd8edb2e
Muhammed Basiouny <muhammad.mouawad@gmail.com>
2023-02-08 02:38:29 +0200

    area of a circle

---
+++ src/areaOfACircle.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  double PI = 3.141592653;
+  double num;
+  double area;
+  cin >> num;
+  area = PI * (num * num);
+  cout << fixed << setprecision(9) << area;
+}
