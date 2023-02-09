152fb6fc4abe6282b31ee83e174e61208beb075f
Muhammed Basiouny <muhammad.mouawad@gmail.com>
2023-02-09 07:34:23 +0200

    Welcome for you with Conditions

---
+++ src/welcomeForYouWithConditions.cpp
@@ -0,0 +1,12 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/I
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int num1, num2;
+  cin >> num1 >> num2;
+  if (num1 >= num2) {
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
+}
