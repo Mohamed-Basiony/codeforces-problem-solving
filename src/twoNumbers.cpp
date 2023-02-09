9106c9054d340cbca53535065643b6d6337d551d
Muhammed Basiouny <muhammad.mouawad@gmail.com>
2023-02-09 07:24:52 +0200

    two numbers

---
+++ src/twoNumbers.cpp
@@ -0,0 +1,13 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/H
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  double num1, num2;
+  cin >> num1 >> num2;
+  cout << "floor " << num1 << " / " << num2 << " = " << floor(num1 / num2)
+       << endl;
+  cout << "ceil " << num1 << " / " << num2 << " = " << ceil(num1 / num2)
+       << endl;
+  cout << "round " << num1 << " / " << num2 << " = " << round(num1 / num2)
+       << endl;
+}
