d9a3cac7b2634813b54dcb65b64101c7362a2b2d
Muhammed Basiouny <muhammad.mouawad@gmail.com>
2023-02-10 06:12:39 +0200

    The Brothers

---
+++ src/theBrothers.cpp
@@ -0,0 +1,13 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/L
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  string namePerson1, namePerson2, nameDad1, nameDad2;
+  cin >> namePerson1 >> nameDad1 >> namePerson2 >> nameDad2;
+
+  if (nameDad1 == nameDad2) {
+    cout << "ARE Brothers";
+  } else {
+    cout << "NOT";
+  }
+}
