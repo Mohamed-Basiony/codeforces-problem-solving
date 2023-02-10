6bb2956203f620be288439f2ca8cd9301dd0bbd9
Muhammed Basiouny <muhammad.mouawad@gmail.com>
2023-02-10 06:41:47 +0200

    Capital or Small or Digit

---
+++ src/capitalOrSmallOrDigit.cpp
@@ -0,0 +1,16 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/M
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  char myChar;
+  cin >> myChar;
+  if (myChar >= 97 && myChar <= 122) {
+    cout << "ALPHA" << endl;
+    cout << "IS SMALL";
+  } else if (myChar >= 65 && myChar <= 90) {
+    cout << "ALPHA" << endl;
+    cout << "IS CAPITAL";
+  } else {
+    cout << "IS DIGIT";
+  }
+}
